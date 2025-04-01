b1156fc656c17e8668b600b533d116e83c9d1a88
leodoneDev <leodone125@gmail.com>
2025-04-02 04:34:54 +0700

    theme customize

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -22,7 +22,7 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'local' );
-12323434
+12323434asdfasd
 /** Database username */
 define( 'DB_USER', 'root' );
 
